2377e56588394e3826c7934986a0b6bb0a07bca7
David E Melnikoff <davidemelnikoff@gmail.com>
2023-04-18 17:01:45 -0400

    initial push

--- jsFiles/dots.js
+++ jsFiles/dots.js
@@ -1,9 +1,22 @@
-
-
 var dotsTask = (function() {
 
 
     var p = {};
+
+    const settings = {
+        responseKeys: ['e', 'i'],
+        signal: 10,
+        noise: 10,
+        nDots: 100,
+        nRounds: 2,
+        breakLength: 10,
+    };
+
+    jsPsych.data.addProperties({
+        signal: settings.signal,
+        noise: settings.noise,
+        nDots: settings.nDots,
+    });
 
 
    /*
@@ -13,7 +26,58 @@ var dotsTask = (function() {
     */
 
 
-    p.intro = {}
+    p.inst = {}
+
+    const pages = {
+        prePractice: [
+            `<div class='parent'>
+                <p>Welcome to Dot Detective!</p>
+                <p>In Dot Detective, you'll see a series of grids. Each grid will contain <span style="color: red">red</span> dots and <span style="color: green">green</span> dots.
+                <br>The number of dots will change over time.</p>
+                <p>Sometimes, the average number of <span style="color: red">red</span> dots will be greater than the average number of <span style="color: green">green</span> dots.</p>
+                <p>Other times, the average number of <span style="color: green">green</span> dots will be greater than the average number of <span style="color: red">red</span> dots.</p>
+                <p><strong>Your job is to detect whether there are more <span style="color: red">red dots</span> or <span style="color: green">green dots</span> on average.</strong></p>
+            </div>`,
+
+            `<div class='parent'>
+                <p>To get a feel for Dot Detective, you will complete a series of practice rounds.</p>
+                <p>Continue when you are ready to begin practicing Dot Detective.</p>
+            </div>`],
+
+        postPractice: [
+            `<div class='parent'>
+                <p>Practice is now complete!</p>
+                <p>Next, you will play ${settings.nRounds} rounds of Dot Detective.</p>
+                <p>Continue to begin Round 1</p>
+            </div>`],
+
+        postTask: [
+            `<div class='parent'>
+                <p>Dot Detective is now complete!</p>
+                <p>To finish this study, please continue to answer a few final questions.</p>
+            </div>`]
+    };
+
+    p.inst.prePractice = {
+        type: jsPsychInstructions,
+        pages: pages.prePractice,
+        show_clickable_nav: true,
+        post_trial_gap: 500,
+    };
+
+    p.inst.postPractice = {
+        type: jsPsychInstructions,
+        pages: pages.postPractice,
+        show_clickable_nav: true,
+        post_trial_gap: 500,
+    };
+
+    p.inst.postTask = {
+        type: jsPsychInstructions,
+        pages: pages.postTask,
+        show_clickable_nav: true,
+        post_trial_gap: 500,
+    };
 
     
    /*
@@ -24,40 +88,56 @@ var dotsTask = (function() {
 
     p.task = {}
 
-    const signalAbs_block1 = 5;
+    let round = 0  // track current round
+    
+    const secondsLeft = arrayToList( (Array.from(Array(settings.breakLength).keys())).map((x) => settings.breakLength - x) )  // list of seconds remaining during breaks
+    
+    const factors = {
+        drift: [settings.signal, -settings.signal],
+        noise: [settings.noise],
+        trialType: [].concat(Array(6).fill('normal'), ['zigZag', 'flatLine']),
+        blockType: ['test'],
+    };  // factors for making experimental design
+    
+    const factorsPractice = {
+        drift: [settings.signal, -settings.signal],
+        noise: [settings.noise],
+        trialType: Array(10).fill('normal'),
+        blockType: ['practice'],
+    };  // factors for making practice block
 
-    const noise_block1 = 10
+    const design = jsPsych.randomization.factorial(factors, 2);  // experimental design
+    
+    const designPractice = jsPsych.randomization.factorial(factorsPractice, 1);  // experimental design for practice block
 
-    const factors_block1 = {
-        signal: [signalAbs_block1, -signalAbs_block1],
-        noise: [noise_block1*.5, noise_block1*2].concat(Array(8).fill(noise_block1)),
-    };
-
-    const design_block1 = jsPsych.randomization.factorial(factors_block1, 3);
-
+    // trials
     const probe = {
         type: jsPsychCanvasKeyboardResponse,
         stimulus: function(c) {
-            dots(c, jsPsych.timelineVariable('signal'), jsPsych.timelineVariable('noise'));
+            dots(c, jsPsych.timelineVariable('drift'), jsPsych.timelineVariable('noise'), jsPsych.timelineVariable('trialType'), settings.responseKeys, settings.nDots);
         },
         canvas_size: [600, 800],
-        choices: ['e','i'],
-        prompt: '<p>Which cloud has more dots on average?</p><p>Press "e" for left and "i" for right.</p>',
-        data: {signal: jsPsych.timelineVariable('signal'), noise: jsPsych.timelineVariable('noise')},
+        choices: settings.responseKeys,
+        prompt: '<p>On average, on there more <span style="color: red">red</span> dots or <span style="color: green">green</span> dots?</p><p>Press <span style="color: red">"e" for red</span> and <span style="color: green">"i" for green</span>.</p>',
+        data: {drift: jsPsych.timelineVariable('drift'), trialType: jsPsych.timelineVariable('trialType'), blockType: jsPsych.timelineVariable('blockType')},
         on_finish: function(data){
-            if(jsPsych.timelineVariable('signal') > 0) {
+            data.round = round;
+            if(jsPsych.timelineVariable('drift') > 0) {
                 data.response == "i" ? data.correct = true : data.correct = false;
             } else {
                 data.response == "i" ? data.correct = false : data.correct = true;
+            };
+            if(data.rt > 60000) { 
+                data.boot = true;
+                jsPsych.endExperiment("The experiment has ended early due to inactivity.") 
             }
-            console.log(jsPsych.timelineVariable('signal'), data.response, data.correct)
         },
     };
 
     const feedback = {
         type: jsPsychHtmlKeyboardResponse,
         stimulus: function() {
-            if(jsPsych.data.getLastTrialData().values()[0].correct == true) {
+            if(jsPsych.data.getLastTrialData().values()[0].correct) {
                 return `<div style="font-size:60px">Correct!</div>`;
             } else {
                 return `<div style="font-size:60px">Wrong!</div>`;
@@ -65,18 +145,49 @@ var dotsTask = (function() {
         },
         choices: "NO_KEYS",
         trial_duration: 1000,
-    };     
+    };
+
+    const clock = {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: function () {
+            let html = `<div style="font-size:20px">
+                <p>Thank you for playing Round ${round} of Dot Detective.
+                <br>Round ${round + 1} will begin in:</p>
+                <p><span style="color: red; font-size: 40px">${jsPsych.timelineVariable('toc')}</span> seconds.</p>
+            </div>`;
+            return html;
+        },
+        choices: "NO_KEYS",
+        trial_duration: 1000,
+    };
+
+    // timelines
+    const countdown = {
+        timeline: [clock],
+        timeline_variables: secondsLeft,
+        conditional_function: function () {
+            return settings.nRounds != round
+        }
+    };
 
     const trial = {
         timeline: [probe, feedback],
-        repetitions: 5,
         randomize_order: true,
-        timeline_variables: design_block1
+        timeline_variables: design,
+        on_timeline_start: function() {
+            round++
+        }
+    };
+
+    p.task.practice = {
+        timeline: [probe, feedback],
+        randomize_order: true,
+        timeline_variables: designPractice,
     };
 
     p.task.block = {
-        timeline: [trial],
-        repetitions: 5,
+        timeline: [trial, countdown],
+        repetitions: settings.nRounds,
     };
 
    /*
@@ -87,7 +198,83 @@ var dotsTask = (function() {
 
     p.Qs = {};
 
+    p.Qs.consent = {
+        type: jsPsychExternalHtml,
+        url: "./static/consent.html",
+        cont_btn: "start",
+     //   check_fn: check_consent
+    };
+
+    p.Qs.demographics = (function() {
+
+        const gender = {
+            type: jsPsychHtmlButtonResponse,
+            stimulus: '<p>What is your gender?</p>',
+            choices: ['Male', 'Female', 'Other'],
+            on_finish: (data) => {
+                data.gender = data.response;
+            }
+        };
+
+        const age = {
+            type: jsPsychSurveyText,
+            questions: [{prompt: "Age:", name: "age"}],
+            on_finish: (data) => {
+                saveSurveyData(data); 
+            },
+        }; 
+
+        const ethnicity = {
+            type: jsPsychHtmlButtonResponse,
+            stimulus: '<p>What is your race?</p>',
+            choices: ['White / Caucasian', 'Black / African American','Asian / Pacific Islander', 'Hispanic', 'Native American', 'Other'],
+            on_finish: (data) => {
+                data.ethnicity = data.response;
+            }
+        };
+
+        const english = {
+            type: jsPsychHtmlButtonResponse,
+            stimulus: '<p>Is English your native language?:</p>',
+            choices: ['Yes', 'No'],
+            on_finish: (data) => {
+                data.english = data.response;
+            }
+        };  
+
+        const finalWord = {
+            type: jsPsychSurveyText,
+            questions: [{prompt: "Questions? Comments? Complains? Provide your feedback here!", rows: 10, columns: 100, name: "finalWord"}],
+            on_finish: (data) => {
+                saveSurveyData(data); 
+            },
+        }; 
+
+        const demos = {
+            timeline: [gender, age, ethnicity, english, finalWord]
+        };
+
+        return demos;
+
+    }());
+
 
     return p;
 
 }());
+
+
+// create timeline
+const timeline = [
+    dotsTask.Qs.consent, 
+    dotsTask.inst.prePractice, 
+    dotsTask.task.practice, 
+    dotsTask.inst.postPractice, 
+    dotsTask.task.block, 
+    dotsTask.inst.postTask, 
+    dotsTask.Qs.demographics, 
+    save_data];
+
+// initiate timeline
+jsPsych.run(timeline);
+
--- jsFiles/global-funcs.js
+++ jsFiles/global-funcs.js
@@ -1,31 +1,90 @@
-const jsPsych = initJsPsych();
+const completionCode = "CB1K8YPV";
 
-const dots = function(c, signal, noise) {
+const jsPsych = initJsPsych({
+    on_finish: () => {
+        if(!jsPsych.data.get().select('boot').values[0]) {
+            document.body.innerHTML = 
+                `<div align='center' style="margin: 10%">
+                    <p>Thank you for participating!<p>
+                    <b>You will be automatically re-directed to Prolific in a few moments.</b>
+                </div>`;
+            setTimeout(() => { 
+                location.href = `https://app.prolific.co/submissions/complete?cc=${completionCode}`
+            }, 2000);
+        };
+    },
+});
+
+let subject_id = jsPsych.data.getURLVariable("PROLIFIC_PID");
+if (!subject_id) { subject_id = jsPsych.randomization.randomID(10) };
+
+jsPsych.data.addProperties({
+    subject_id: subject_id,
+});
+
+const filename = `${subject_id}.csv`;
+
+const save_data = {
+    type: jsPsychPipe,
+    action: "save",
+    experiment_id: "cSbwduMJ6B7Y",
+    filename: filename,
+    data_string: ()=>jsPsych.data.get().csv()
+};
+
+const saveSurveyData = (data) => {
+    const names = Object.keys(data.response);
+    const values = Object.values(data.response);
+    for(let i = 0; i < names.length; i++) {
+        data[names[i]] = values[i];
+    };      
+};
+
+function arrayToList(array) {
+    let list = Array(array.length);
+    for (let i = array.length - 1; i >= 0; i--) {
+        list[i] = { toc: array[i] };
+    }
+    return list;
+}
+
+const dots = function(c, signal, noise, trialType, responseKeys, nDots) {
     const canvasWidth = c.width;
     const canvasHeight = c.height;
     const ctx = c.getContext('2d');
-    const xMinLeft = canvasWidth * .1;
-    const xMaxLeft = canvasWidth * .4;
-    const yMinLeft = (canvasHeight / 2) - (xMaxLeft - xMinLeft);
-    const yMaxLeft = (canvasHeight / 2) + (xMaxLeft - xMinLeft);
-    const xMinRight = canvasWidth * .6;
-    const xMaxRight = canvasWidth * .9;
-    const yMinRight = (canvasHeight / 2) - (xMaxRight - xMinRight);
-    const yMaxRight = (canvasHeight / 2) + (xMaxRight - xMinRight);
+    const xMin = canvasWidth * .30;
+    const xMax = canvasWidth * .70;
+    const yMin = (canvasHeight - xMax + xMin) / 2;
+    const yMax = (canvasHeight + xMax - xMin) / 2;
+    const nX = 40;
+    const nY = nX;
+    const rad = 2;
+    const xSpace = ((xMax - xMin) / nX) - 2*rad;
+    const ySpace = ((yMax - yMin) / nY) - 2*rad;
+    const pos = {
+        xPos: Array.from(Array(nX + 1).keys(), i => xMin + i * (2*rad + xSpace)),
+        yPos: Array.from(Array(nY + 1).keys(), i => yMin + i * (2*rad + ySpace)),
+    };
+    const posFactorial = jsPsych.randomization.factorial(pos, 1, true);
+    const idx = Array.from({length: posFactorial.xPos.length}, (item, index) => index);
+    let zig = [0, 2][Math.floor(Math.random() * 2)];
+    const zigVec = [0, .5, 0, -.5];
+    let n1 = 0;
+    let n2 = 0;
 
     function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
-        ctx.beginPath()
-        ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
         if (fill) {
-            ctx.fillStyle = fill
-            ctx.fill()
-        }
+            ctx.fillStyle = fill;
+            ctx.fill();
+        };
         if (stroke) {
-            ctx.lineWidth = strokeWidth
-            ctx.strokeStyle = stroke
-            ctx.stroke()
-        }
-    }
+            ctx.lineWidth = strokeWidth;
+            ctx.strokeStyle = stroke;
+            ctx.stroke();
+        };
+    };
 
     function gaussianRandom(mean, stdev) {
         let x = -1;
@@ -33,37 +92,60 @@ const dots = function(c, signal, noise) {
             let u = 1 - Math.random(); // Converting [0,1) to (0,1]
             let v = Math.random();
             let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-            x = z * stdev + mean;
-        }
+            x = Math.floor(z * stdev + mean);
+        };
         return x;
-    }
+    };
 
-    function getRandom(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    function getYRange(min, max, x) {
-        let rad = (max - min) / 2;
-        let a = Math.abs(min + rad - x);
-        let b = Math.sqrt(rad**2 - a**2);
-        return range = [(canvasHeight/2) - b, (canvasHeight/2) + b];
-    }
+    function randomDots() {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight); // Clean up
+        let randIdx = jsPsych.randomization.repeat(idx, 1);
+        let n1 = gaussianRandom(nDots - (signal/2), noise);
+        let n2 = gaussianRandom(nDots + (signal/2), noise);
+        for(i = 0; i < n1; i++) {
+            let xPos1 = posFactorial.xPos[randIdx[i]];
+            let yPos1 = posFactorial.yPos[randIdx[i]];
+            drawCircle(ctx, xPos1, yPos1, rad, 'black', 'red', 2);
+        };
+        for(i = n1; i < (n1 + n2); i++) {
+            let xPos2 = posFactorial.xPos[randIdx[i]];
+            let yPos2 = posFactorial.yPos[randIdx[i]];
+            drawCircle(ctx, xPos2, yPos2, rad, 'black', 'green', 2);
+        };
+    };
 
     const id = setInterval(() => {
         ctx.clearRect(0, 0, canvasWidth, canvasHeight); // Clean up
-        let nLeft = gaussianRandom(30 - (signal/2), noise);
-        let nRight = gaussianRandom(30 + (signal/2), noise);
-        for(i = 1; i <= nLeft; i++) {
-            let xPosLeft = getRandom(xMinLeft, xMaxLeft);
-            let yRangeLeft = getYRange(xMinLeft, xMaxLeft, xPosLeft);
-            let yPosLeft = getRandom(yRangeLeft[0], yRangeLeft[1]);
-            drawCircle(ctx, xPosLeft, yPosLeft, 2, 'black', 'red', 2);
+        let randIdx = jsPsych.randomization.repeat(idx, 1);
+        if (trialType == 'zigZag') {
+            n1 = nDots - (signal*zigVec[zig]);
+            n2 = nDots + (signal*zigVec[zig]);
+        } else if (trialType == 'flatLine') {
+            n1 = nDots;
+            n2 = nDots;
+        } else {
+            n1 = gaussianRandom(nDots - (signal/2), noise);
+            n2 = gaussianRandom(nDots + (signal/2), noise); 
+        };
+        for(i = 0; i < n1; i++) {
+            let xPos1 = posFactorial.xPos[randIdx[i]];
+            let yPos1 = posFactorial.yPos[randIdx[i]];
+            drawCircle(ctx, xPos1, yPos1, rad, 'black', 'red', 2);
+        };
+        for(i = n1; i < (n1 + n2); i++) {
+            let xPos2 = posFactorial.xPos[randIdx[i]];
+            let yPos2 = posFactorial.yPos[randIdx[i]];
+            drawCircle(ctx, xPos2, yPos2, rad, 'black', 'green', 2);
+        };
+        zig++;
+        if (zig == zigVec.length) { zig = 0 };
+    }, 100);
+
+
+    addEventListener("keydown", (e) => {
+        if (responseKeys.includes(e.key)) {
+            clearInterval(id);
         }
-        for(i = 1; i <= nRight; i++) {
-            let xPosRight = getRandom(xMinRight, xMaxRight);
-            let yRangeRight = getYRange(xMinRight, xMaxRight, xPosRight);
-            let yPosRight = getRandom(yRangeRight[0], yRangeRight[1]);
-            drawCircle(ctx, xPosRight, yPosRight, 2, 'black', 'red', 2);  
-        }
-    }, 150);
+    })
+
 };
